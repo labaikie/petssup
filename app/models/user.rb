d1a75101b217c55cf905a84e968dd2f4fef67342
La Baik <labaik237@gmail.com>
2015-12-18 22:02:06 -0800

    adding Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-
   validates :email, presence: true, uniqueness: { case_sensitive: false}
 
+  has_many :friendships
+  has_many :friends, through: :friendships
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218024612) do
+ActiveRecord::Schema.define(version: 20151219055932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "friendships", ["user_id"], name: "index_friendships_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                                                null: false
@@ -36,4 +45,5 @@ ActiveRecord::Schema.define(version: 20151218024612) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
+  add_foreign_key "friendships", "users"
 end
