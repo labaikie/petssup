854bc3a5e41fe36d753196d2282ac1f0749e99df
La Baik <labaik237@gmail.com>
2016-01-05 15:07:05 -0800

    adding users to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+cotton = User.create(
+  name: "Cotton",
+  email: "cotton@server.com",
+  password: "cotton1234",
+  password_confirmation: "cotton1234",
+  family_name: "Lee",
+  animal_type: "Cotton de Tulear",
+  gender: "male",
+  human_name: "Yeon"
+  )
+
+ziola = User.create(
+  name: "Ziola",
+  email: "ziola@server.com",
+  password: "ziola1234",
+  password_confirmation: "ziola1234",
+  family_name: "Oh",
+  animal_type: "Dachshund",
+  gender: "male",
+  human_name: "June"
+  )
+
+latte = User.create(
+  name: "Latte",
+  email: "latte@server.com",
+  password: "latte1234",
+  password_confirmation: "latte1234",
+  family_name: "Ha",
+  animal_type: "Kitten",
+  gender: "female",
+  human_name: "Eunmi"
+  )
