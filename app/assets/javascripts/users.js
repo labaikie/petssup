e34781772a187114a14c399913221a6c0ce8d9e3
La Baik <labaik237@gmail.com>
2016-01-05 17:10:43 -0800

    add new user and new session modal at home page

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -14,6 +14,14 @@ $('#edit-post-btn').click(function(){
   hideAndshow('#edit-profile','#show-profile')
 });
 
+$('#login-on-join').click(function(){
+  hideAndshow('#joinDp','#loginDp')
+});
+
+$('#join-on-login').click(function(){
+  hideAndshow('#loginDp','#joinDp')
+});
+
 function hideAndShow(hide, show) {
   $(hide).css('display', 'none');
   $(show).css('display', 'block')
@@ -21,11 +29,15 @@ function hideAndShow(hide, show) {
 
 $('#comments-btn').click(function(){
   showHidden('.show-comments')
-})
+});
+
+$('#join').click(function(){
+  showHidden('#joinDp')
+});
 
 $('#login').click(function(){
   showHidden('#loginDp')
-})
+});
 
 function showHidden(hidden){
   $(hidden).toggle();
