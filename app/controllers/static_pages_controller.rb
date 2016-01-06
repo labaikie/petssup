c4f9de7810e39f6571a5f49c5bbed53425ee0489
La Baik <labaik237@gmail.com>
2016-01-05 20:13:50 -0800

    add the Guardian API on home page with styling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,9 @@ class StaticPagesController < ApplicationController
   def home
       @random = grab_random
       @user = User.new
+      data = HTTParty.get("http://content.guardianapis.com/search?q=pet&pets&animal&lifestyle&from-date=2015-12-01&api-key=8327ea7e-bc39-4935-a195-90e140c7c7e8")
+      response = JSON.parse(data.body)
+      @news = response['response']['results'][0..10]
   end
 
   def grab_random
